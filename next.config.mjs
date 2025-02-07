b95d01dc5ed5e42afc256d63efe3cec59963571d
dipak-trio-spire <dipak@trio-spire.com>
2025-02-07 15:01:20 +0530

    nextconfing change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ["s3-us-west-2.amazonaws.com", "scontent.cdninstagram.com" , "scontent-iad3-2.cdninstagram.com"], // Add this line
+      remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.cdninstagram.com",
+      },
+    ],
+      domains: ["s3-us-west-2.amazonaws.com", "scontent.cdninstagram.com" ,"scontent-iad3-1.cdninstagram.com", "scontent-iad3-2.cdninstagram.com"], // Add this line
     },
   };
   
