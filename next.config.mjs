977d502c3499b5b9f291fc478fbfc8f2a6ea27d8
dipak-trio-spire <dipak@trio-spire.com>
2025-02-07 14:56:13 +0530

    nextconfing change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ["s3-us-west-2.amazonaws.com", "scontent.cdninstagram.com"], // Add this line
+      domains: ["s3-us-west-2.amazonaws.com", "scontent.cdninstagram.com" , "scontent-iad3-2.cdninstagram.com"], // Add this line
     },
   };
   
